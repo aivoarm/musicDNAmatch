2f840a202bac9732792d6a743856bb348fbfeaa7
Arman Ayva <aayvazy@gmail.com>
2026-03-12 19:24:05 -0400

    Updated Genra's from major spotify lists

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,8 @@ import { signOut } from "@workos-inc/authkit-nextjs";
 import { cookies } from "next/headers";
 export const runtime = "edge";
 
-export async function GET() {
+export async function GET(request: Request) {
+    const { origin } = new URL(request.url);
     const cookieStore = await cookies();
 
     // Clear application-specific "cache" cookies
@@ -12,6 +13,5 @@ export async function GET() {
     cookieStore.delete("auth_email");
     cookieStore.delete("last_spotify_url");
 
-    return await signOut({ returnTo: "/" });
-
+    return await signOut({ returnTo: origin });
 }
